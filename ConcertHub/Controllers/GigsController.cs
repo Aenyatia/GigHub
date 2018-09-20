0746e25aa676b972f6ea6e3e069408c74f1b89f9
Aenyatia <aenyatia@gmail.com>
2018-09-20 12:18:13 +0200

    Add gig form.

---
+++ ConcertHub/Controllers/GigsController.cs
@@ -0,0 +1,25 @@
+﻿using ConcertHub.Infrastructure.Data;
+using ConcertHub.ViewModels;
+using Microsoft.AspNetCore.Mvc;
+using System.Linq;
+
+namespace ConcertHub.Controllers
+{
+	public class GigsController : Controller
+	{
+		private readonly ConcertContext _context;
+
+		public GigsController(ConcertContext context)
+		{
+			_context = context;
+		}
+
+		[HttpGet]
+		public IActionResult Create()
+		{
+			var viewModel = new GigFormViewModel { Genres = _context.Genres.ToList() };
+
+			return View(viewModel);
+		}
+	}
+}
